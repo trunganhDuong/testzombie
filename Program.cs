1c729dc7037d452524a7ad4867e0f5602a3f5190
AnhDT1 <anhdt1@daivietgroup.net.vn>
2020-06-02 09:46:47 +0700

    initialize

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace TestZombie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("Read file");
+                File.ReadAllText("");
+            }
+        }
+    }
+}
